d83d8f13574f34b61eae700f99b183f9c9b91663
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-06 16:30:11 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,9 @@
 class Book
-  attr_accessor :author :page_count :genre
+  attr_accessor :author, :page_count, :genre
   attr_reader :title
 
   def initialize (title)
     @title = title
   end
-  
-end
 
+end
